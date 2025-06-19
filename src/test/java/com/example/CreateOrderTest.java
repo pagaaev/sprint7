81ebe23e604a78ed4f9ed2fa67734ffe46f247f7
Artur Pagaev <paagaev@mail.ru>
2025-06-19 09:38:07 +0700

    Refactor: вынес класс Order в отдельный файл для улучшения структуры проекта

--- src/test/java/com/example/CreateOrderTest.java
+++ src/test/java/com/example/CreateOrderTest.java
@@ -1,5 +1,6 @@
 package com.example;
 
+import com.example.model.Order;
 import io.qameta.allure.Description;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
@@ -66,76 +67,5 @@ public class CreateOrderTest {
         order.setComment("Saske, come back to Konoha");
         order.setColor(colors);
         return order;
-    }
-
-    // Внутренний класс для представления заказа
-    private static class Order {
-        private String firstName;
-        private String lastName;
-        private String address;
-        private String metroStation;
-        private String phone;
-        private int rentTime;
-        private String deliveryDate;
-        private String comment;
-        private List<String> color;
-
-        // Геттеры
-        public String getFirstName() {
-            return firstName;
-        }
-        public String getLastName() {
-            return lastName;
-        }
-        public String getAddress() {
-            return address;
-        }
-        public String getMetroStation() {
-            return metroStation;
-        }
-        public String getPhone() {
-            return phone;
-        }
-        public int getRentTime() {
-            return rentTime;
-        }
-        public String getDeliveryDate() {
-            return deliveryDate;
-        }
-        public String getComment() {
-            return comment;
-        }
-        public List<String> getColor() {
-            return color;
-        }
-
-        // Сеттеры
-        public void setFirstName(String firstName) {
-            this.firstName = firstName;
-        }
-        public void setLastName(String lastName) {
-            this.lastName = lastName;
-        }
-        public void setAddress(String address) {
-            this.address = address;
-        }
-        public void setMetroStation(String metroStation) {
-            this.metroStation = metroStation;
-        }
-        public void setPhone(String phone) {
-            this.phone = phone;
-        }
-        public void setRentTime(int rentTime) {
-            this.rentTime = rentTime;
-        }
-        public void setDeliveryDate(String deliveryDate) {
-            this.deliveryDate = deliveryDate;
-        }
-        public void setComment(String comment) {
-            this.comment = comment;
-        }
-        public void setColor(List<String> color) {
-            this.color = color;
-        }
     }
 }
